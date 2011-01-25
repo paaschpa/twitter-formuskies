e7aba945e436f22e10b1702860a8a95e4da144bd
unknown <ppaasch@.(none)>
2011-01-24 22:18:05 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterForMuskies::Application.config.secret_token = '49758409f2355c5840460b21e030fe34b07f7da6cfa0667d072fb3ce8f8b00d92dffcba131b3863801f71f0111a2537153efe0a6c78cbe3ce527b038fd8da386'
